8fd9b8935d565ba0ffa6bad5e03e2386850b257f
Jundertag <jaydenmacarthur3@gmail.com>
2026-02-19 22:07:10 -0400

    Notification IDs now changed based on device name and address

--- app/src/main/java/com/jayden/bluetoothalerts/app/receivers/BluetoothDeviceEventReceiver.kt
+++ app/src/main/java/com/jayden/bluetoothalerts/app/receivers/BluetoothDeviceEventReceiver.kt
@@ -24,18 +24,20 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                     return
                 }
 
-                val bluetoothTransport = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                    intent.getIntExtra(BluetoothDevice.EXTRA_TRANSPORT, BluetoothDevice.ERROR)
-                } else null
+                val bluetoothTransport =
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        intent.getIntExtra(BluetoothDevice.EXTRA_TRANSPORT, BluetoothDevice.ERROR)
+                    } else null
                 if (bluetoothTransport == BluetoothDevice.ERROR) {
                     Log.i(TAG, "Bluetooth Device Transport returned an error, ignoring")
                     return
                 }
 
                 val deviceAddress = bluetoothDevice.address
-                val deviceName: String? = if (context.checkSelfPermission(Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
-                    bluetoothDevice.name
-                } else "<missing-permission BLUETOOTH_CONNECT>"
+                val deviceName: String? =
+                    if (context.checkSelfPermission(Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
+                        bluetoothDevice.name
+                    } else "<missing-permission BLUETOOTH_CONNECT>"
                 val deviceAlias = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
                     if (context.checkSelfPermission(Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
                         bluetoothDevice.alias
@@ -51,13 +53,16 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                 Log.i(TAG, "Notifying user of Bluetooth Acl Connected event.")
                 AppNotificationManager.showBluetoothAclConnectedNotification(
                     context,
-                    AppNotificationManager.BLUETOOTH_ACL_CONNECTED_NOTIFY_ID,
+                    AppNotificationManager.BLUETOOTH_ACL_CONNECTED_NOTIFY_ID + deviceAddress.hashCode() + deviceName.let {
+                        it?.hashCode() ?: 0
+                    },
                     deviceAddress,
                     deviceName,
                     deviceAlias,
                     deviceTransport
                 )
             }
+
             BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED -> {
                 val bluetoothDevice = IntentHelper.getBluetoothDevice(intent)
 
@@ -82,12 +87,15 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                 Log.i(TAG, "Notifying user of Bluetooth Acl Disconnect Requested event.")
                 AppNotificationManager.showBluetoothAclDisconnectRequestedNotification(
                     context,
-                    AppNotificationManager.BLUETOOTH_ACL_DISCONNECT_REQUESTED_NOTIFY_ID,
+                    AppNotificationManager.BLUETOOTH_ACL_DISCONNECT_REQUESTED_NOTIFY_ID + deviceAddress.hashCode() + deviceName.let {
+                        it?.hashCode() ?: 0
+                    },
                     deviceAddress,
                     deviceName,
                     deviceAlias
                 )
             }
+
             BluetoothDevice.ACTION_ACL_DISCONNECTED -> {
                 val bluetoothDevice = IntentHelper.getBluetoothDevice(intent)
 
@@ -112,12 +120,15 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                 Log.i(TAG, "Notifying user of Bluetooth Acl Disconnected event.")
                 AppNotificationManager.showBluetoothAclDisconnectedNotification(
                     context,
-                    AppNotificationManager.BLUETOOTH_ACL_DISCONNECTED_NOTIFY_ID,
+                    AppNotificationManager.BLUETOOTH_ACL_DISCONNECTED_NOTIFY_ID + deviceAddress.hashCode() + deviceName.let {
+                        it?.hashCode() ?: 0
+                    },
                     deviceAddress,
                     deviceName,
                     deviceAlias
                 )
             }
+
             BluetoothDevice.ACTION_ALIAS_CHANGED -> {
                 val bluetoothDevice = IntentHelper.getBluetoothDevice(intent)
 
@@ -142,12 +153,15 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                 Log.i(TAG, "Notifying user of Bluetooth Device Alias Changed event.")
                 AppNotificationManager.showBluetoothAliasChangedNotification(
                     context,
-                    AppNotificationManager.BLUETOOTH_ALIAS_NOTIFY_ID,
+                    AppNotificationManager.BLUETOOTH_ALIAS_NOTIFY_ID + deviceAddress.hashCode() + deviceName.let {
+                        it?.hashCode() ?: 0
+                    },
                     deviceAddress,
                     deviceName,
                     deviceAlias
                 )
             }
+
             BluetoothDevice.ACTION_BOND_STATE_CHANGED -> {
                 val bluetoothDevice = IntentHelper.getBluetoothDevice(intent)
 
@@ -156,7 +170,8 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                     return
                 }
 
-                val bluetoothBondState = intent.getIntExtra(BluetoothDevice.EXTRA_BOND_STATE, BluetoothDevice.ERROR)
+                val bluetoothBondState =
+                    intent.getIntExtra(BluetoothDevice.EXTRA_BOND_STATE, BluetoothDevice.ERROR)
                 if (bluetoothBondState == BluetoothDevice.ERROR) {
                     Log.i(TAG, "Bluetooth Bond State returned an error, ignoring")
                     return
@@ -167,17 +182,21 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                     if (context.checkSelfPermission(Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
                         bluetoothDevice.name
                     } else "<missing-permission BLUETOOTH_CONNECT>"
-                val deviceBondState = AppNotificationManager.BluetoothBondState.fromId(bluetoothBondState)!!
+                val deviceBondState =
+                    AppNotificationManager.BluetoothBondState.fromId(bluetoothBondState)!!
 
                 Log.i(TAG, "Notifying user of Bluetooth Device Bond State Changed event.")
                 AppNotificationManager.showBluetoothBondStateChangedNotification(
                     context,
-                    AppNotificationManager.BLUETOOTH_BOND_STATE_NOTIFY_ID,
+                    AppNotificationManager.BLUETOOTH_BOND_STATE_NOTIFY_ID + deviceAddress.hashCode() + deviceName.let {
+                        it?.hashCode() ?: 0
+                    },
                     deviceAddress,
                     deviceName,
                     deviceBondState
                 )
             }
+
             BluetoothDevice.ACTION_CLASS_CHANGED -> {
                 val bluetoothDevice = IntentHelper.getBluetoothDevice(intent)
 
@@ -187,7 +206,10 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                 }
 
                 val bluetoothClass = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                    intent.getParcelableExtra(BluetoothDevice.EXTRA_CLASS, BluetoothClass::class.java)
+                    intent.getParcelableExtra(
+                        BluetoothDevice.EXTRA_CLASS,
+                        BluetoothClass::class.java
+                    )
                 } else {
                     @Suppress("DEPRECATION")
                     intent.getParcelableExtra<BluetoothClass>(BluetoothDevice.EXTRA_CLASS)
@@ -207,7 +229,9 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                 Log.i(TAG, "Notifying user of Bluetooth Device Class Changed event.")
                 AppNotificationManager.showBluetoothClassChangedNotification(
                     context,
-                    AppNotificationManager.BLUETOOTH_CLASS_NOTIFY_ID,
+                    AppNotificationManager.BLUETOOTH_CLASS_NOTIFY_ID + deviceAddress.hashCode() + deviceName.let {
+                        it?.hashCode() ?: 0
+                    },
                     deviceAddress,
                     deviceName,
                     AppNotificationManager.BluetoothDeviceClassMajor.fromId(bluetoothClass.majorDeviceClass)!!,
@@ -215,6 +239,7 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                 )
 
             }
+
             BluetoothDevice.ACTION_ENCRYPTION_CHANGE -> {
                 val bluetoothDevice = IntentHelper.getBluetoothDevice(intent)
 
@@ -224,7 +249,10 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                 }
 
                 val encryptionStatus = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.BAKLAVA) {
-                    intent.getIntExtra(BluetoothDevice.EXTRA_ENCRYPTION_STATUS, BluetoothDevice.ERROR)
+                    intent.getIntExtra(
+                        BluetoothDevice.EXTRA_ENCRYPTION_STATUS,
+                        BluetoothDevice.ERROR
+                    )
                 } else null
                 if (encryptionStatus == BluetoothDevice.ERROR) {
                     Log.i(TAG, "Bluetooth Encryption Status returned an error, ignoring")
@@ -251,8 +279,12 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                     return
                 }
 
-                val encryptionAlgorithm = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.BAKLAVA) {
-                        intent.getIntExtra(BluetoothDevice.EXTRA_ENCRYPTION_ALGORITHM, BluetoothDevice.ERROR)
+                val encryptionAlgorithm =
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.BAKLAVA) {
+                        intent.getIntExtra(
+                            BluetoothDevice.EXTRA_ENCRYPTION_ALGORITHM,
+                            BluetoothDevice.ERROR
+                        )
                     } else null
                 if (encryptionAlgorithm == BluetoothDevice.ERROR) {
                     Log.i(TAG, "Bluetooth Encryption Algorithm returned an error, ignoring")
@@ -272,16 +304,21 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                 Log.i(TAG, "Notifying user of Bluetooth Encryption Changed event.")
                 AppNotificationManager.showBluetoothEncryptionChangedNotification(
                     context,
-                    AppNotificationManager.BLUETOOTH_ENCRYPTION_NOTIFY_ID,
+                    AppNotificationManager.BLUETOOTH_ENCRYPTION_NOTIFY_ID + deviceAddress.hashCode() + deviceName.let {
+                        it?.hashCode() ?: 0
+                    },
                     deviceAddress,
                     deviceName,
                     encryptionStatus,
                     BluetoothHCIErrorCode.lookupCode(encryptionStatus),
                     encryptionEnabled == true,
                     keySize * 8,
-                    AppNotificationManager.BluetoothDeviceEncryptionAlgorithm.fromId(encryptionAlgorithm)!!
+                    AppNotificationManager.BluetoothDeviceEncryptionAlgorithm.fromId(
+                        encryptionAlgorithm
+                    )!!
                 )
             }
+
             BluetoothDevice.ACTION_FOUND -> {
                 val bluetoothDevice = IntentHelper.getBluetoothDevice(intent)
 
@@ -305,13 +342,18 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                     return
                 }
 
-                val bluetoothRssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE)
+                val bluetoothRssi =
+                    intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE)
 
-                val deviceCoordinatedMember = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                    intent.getBooleanExtra(BluetoothDevice.EXTRA_IS_COORDINATED_SET_MEMBER, false)
-                } else {
-                    null
-                }
+                val deviceCoordinatedMember =
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        intent.getBooleanExtra(
+                            BluetoothDevice.EXTRA_IS_COORDINATED_SET_MEMBER,
+                            false
+                        )
+                    } else {
+                        null
+                    }
 
                 val deviceRssi = if (bluetoothRssi != Short.MIN_VALUE) {
                     bluetoothRssi
@@ -326,7 +368,9 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                 Log.i(TAG, "Notifying user of Bluetooth Device Found event.")
                 AppNotificationManager.showBluetoothDeviceFoundNotification(
                     context,
-                    AppNotificationManager.BLUETOOTH_FOUND_NOTIFY_ID,
+                    AppNotificationManager.BLUETOOTH_FOUND_NOTIFY_ID + deviceAddress.hashCode() + deviceName.let {
+                        it?.hashCode() ?: 0
+                    },
                     deviceAddress,
                     deviceName,
                     AppNotificationManager.BluetoothDeviceClassMajor.fromId(bluetoothClass.majorDeviceClass)!!,
@@ -334,6 +378,7 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                     deviceCoordinatedMember
                 )
             }
+
             BluetoothDevice.ACTION_KEY_MISSING -> {
                 val bluetoothDevice = IntentHelper.getBluetoothDevice(intent)
 
@@ -342,9 +387,13 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                     return
                 }
 
-                val bluetoothBondLossReason = if (Build.VERSION.SDK_INT_FULL >= Build.VERSION_CODES_FULL.BAKLAVA_1) {
-                    intent.getIntExtra(BluetoothDevice.EXTRA_BOND_LOSS_REASON, BluetoothDevice.ERROR)
-                } else null
+                val bluetoothBondLossReason =
+                    if (Build.VERSION.SDK_INT_FULL >= Build.VERSION_CODES_FULL.BAKLAVA_1) {
+                        intent.getIntExtra(
+                            BluetoothDevice.EXTRA_BOND_LOSS_REASON,
+                            BluetoothDevice.ERROR
+                        )
+                    } else null
 
                 if (bluetoothBondLossReason == BluetoothDevice.ERROR) {
                     Log.i(TAG, "Bluetooth Bond Loss Reason returned an error, ignoring")
@@ -357,12 +406,19 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                 Log.i(TAG, "Notifying user of Bluetooth Device Key Missing event.")
                 AppNotificationManager.showBluetoothKeyMissingNotification(
                     context,
-                    AppNotificationManager.BLUETOOTH_MISSING_KEY_NOTIFY_ID,
+                    AppNotificationManager.BLUETOOTH_MISSING_KEY_NOTIFY_ID + deviceAddress.hashCode() + deviceName.let {
+                        it?.hashCode() ?: 0
+                    },
                     deviceAddress,
                     deviceName,
-                    bluetoothBondLossReason?.let { AppNotificationManager.BluetoothBondLossReason.fromId(it) }
+                    bluetoothBondLossReason?.let {
+                        AppNotificationManager.BluetoothBondLossReason.fromId(
+                            it
+                        )
+                    }
                 )
             }
+
             BluetoothDevice.ACTION_NAME_CHANGED -> {
                 val bluetoothDevice = IntentHelper.getBluetoothDevice(intent)
 
@@ -377,11 +433,14 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                 Log.i(TAG, "Notifying user of Bluetooth Device Name Changed event.")
                 AppNotificationManager.showBluetoothNameChangedNotification(
                     context,
-                    AppNotificationManager.BLUETOOTH_NAME_NOTIFY_ID,
+                    AppNotificationManager.BLUETOOTH_NAME_NOTIFY_ID + deviceAddress.hashCode() + deviceName.let {
+                        it?.hashCode() ?: 0
+                    },
                     deviceAddress,
                     deviceName
                 )
             }
+
             BluetoothDevice.ACTION_PAIRING_REQUEST -> {
                 val bluetoothDevice = IntentHelper.getBluetoothDevice(intent)
 
@@ -402,12 +461,15 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                 Log.i(TAG, "Notifying user of Bluetooth Device Pairing Request event.")
                 AppNotificationManager.showBluetoothPairingRequestNotification(
                     context,
-                    AppNotificationManager.BLUETOOTH_PAIRING_REQUEST_NOTIFY_ID,
+                    AppNotificationManager.BLUETOOTH_PAIRING_REQUEST_NOTIFY_ID + deviceAddress.hashCode() + deviceName.let {
+                        it?.hashCode() ?: 0
+                    },
                     deviceAddress,
                     deviceName,
                     bluetoothPairingKey
                 )
             }
+
             BluetoothDevice.ACTION_UUID -> {
                 val bluetoothDevice = IntentHelper.getBluetoothDevice(intent)
 
@@ -424,12 +486,15 @@ class BluetoothDeviceEventReceiver : BroadcastReceiver() {
                 Log.i(TAG, "Notifying user of Bluetooth Device UUID event.")
                 AppNotificationManager.showBluetoothUuidNotification(
                     context,
-                    AppNotificationManager.BLUETOOTH_UUID_NOTIFY_ID,
+                    AppNotificationManager.BLUETOOTH_UUID_NOTIFY_ID + deviceAddress.hashCode() + deviceName.let {
+                        it?.hashCode() ?: 0
+                    },
                     deviceAddress,
                     deviceName,
                     bluetoothDeviceUuids
                 )
             }
+
             else -> {}
         }
     }
